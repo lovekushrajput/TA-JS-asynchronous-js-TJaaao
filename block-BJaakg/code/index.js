102748f47e13a922ee1d85ecfa61076b91e37cf8
lovekush rajput <lovekushrazput143@gmail.com>
2022-03-07 08:44:11 +0530

    changes

--- block-BJaakg/code/index.js
+++ block-BJaakg/code/index.js
@@ -1,79 +1,70 @@
-let main = document.querySelector('.main');
-
+(function(){
+    let main = document.querySelector('.main');
 let popup = document.querySelector('.popup');
 let gotPeople = document.querySelector('.peoples');
-let characters = document.querySelector('.chracters');
+let charactersUL = document.querySelector('.chracters');
 let closeBtn = document.querySelector('.closeBtn');
 let loader = document.querySelector('.spinner');
+loader.style.display = 'none';
 let error = document.querySelector('.error');
-let charactersArray;
 
 closeBtn.addEventListener('click', () => {
   main.style.display = 'block';
   popup.style.display = 'none';
-  main.style.position='absolute'
-  main.style.display = 'grid'
+  main.style.position = 'absolute';
+  main.style.display = 'grid';
 });
 
-function charactersUI() {
-  main.style.position='fixed'
-  main.style.right = '14.5%'
-  main.style.width = '70.5%'
-  main.style.zindex = '2';
-  popup.style.display = 'block';
-  popup.style.zindex = '100';
-
-
-  characters.innerHTML = '';
-  charactersArray.map((e) => {
-    //  url of characters
-    let data = fetch(e)
-      .then((res) => {
+function charactersUI(character) {
+  loader.style.display = 'block';
+  loader.style.display = 'flex';
+  Promise.all(
+    character.map((e) =>
+      fetch(e).then((res) => {
         if (res.ok) {
-          if (res) {
-            loader.style.display = 'block';
-            loader.style.display = 'flex';
-            return res.json();
-          }
+          return res.json();
         } else {
           throw new Error(`response is not ok ${res.status}`);
         }
       })
-      .then((r) => r)
-      .catch((err) => (error.innerText = err));
-
-    //    creating UI
-    data
-      .then((val) => {
-        let alises;
-        val.aliases.forEach((elm) => (alises = elm));
-
+    )
+  )
+    .then((charData) => {
+      charactersUL.innerHTML = '';
+      charData.forEach((char) => {
         let li = document.createElement('li');
         li.classList = 'chracterBox';
-        li.innerText = `${val.name} : (${alises})`;
-        characters.append(li);
-      })
-      .catch((err) => (error.innerText = err))
-      .finally(() => {
-        loader.style.display = 'none';
+        li.innerText = `${char.name} : (${char.aliases.join(' ')})`;
+        charactersUL.append(li);
       });
-  });
+    })
+    .catch((err) => (error.innerText = err))
+    .finally(() => {
+      loader.style.display = 'none';
+    });
 }
 
 function createUI(arrayOfPeoples) {
   arrayOfPeoples.forEach((people) => {
-    let autherNames = people.authors[0];
     let li = document.createElement('li');
     li.classList = 'box';
     let h2 = document.createElement('h2');
     h2.innerText = people.name;
     let p = document.createElement('p');
-    p.innerText = autherNames;
+    p.innerText = people.authors.join(' ');
     let a = document.createElement('a');
     a.classList = 'showBtn';
     a.innerText = `Show Characters(${people.characters.length})`;
-    charactersArray = people.characters;
-    a.addEventListener('click', charactersUI);
+
+    a.addEventListener('click', () => {
+      main.style.position = 'fixed';
+      main.style.right = '14.5%';
+      main.style.width = '70.5%';
+      main.style.zindex = '2';
+      popup.style.display = 'block';
+      popup.style.zindex = '100';
+      charactersUI(people.characters);
+    });
     li.append(h2, p, a);
     gotPeople.append(li);
   });
@@ -100,3 +91,5 @@ if (navigator.onLine) {
 } else {
   error.innerText = 'Check Your Internet Connections';
 }
+
+})()
